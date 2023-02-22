3367001b5bba4881d023c0f40c98a30879393be5
Hampus Isebring <hampus.isebring@gmail.com>
2023-02-22 12:53:34 +0100

    first commit

---
+++ src/api/apiConfig.ts
@@ -0,0 +1,13 @@
+interface apiConfig {
+  imgPath: string;
+}
+
+const apiConfig = {
+  baseUrl: "https://api.themoviedb.org/3/",
+  api: "f4dff989aeebe08758854a530c47abf3",
+  originalImage: (imgPath: string) =>
+    `https://image.tmbd.org/t/p/original/${imgPath}`,
+  w500Image: (imgPath: string) => `https://image.tmbd.org//t/p/w500/${imgPath}`,
+};
+
+export default apiConfig;
---
+++ src/api/tmdbApi.ts
@@ -0,0 +1,61 @@
+import axiosClient from "./axiosClient";
+
+export const category: { [key: string]: string } = {
+  movie: "movie",
+  tv: "tv",
+};
+
+export const movieType: { [key: string]: string } = {
+  upcoming: "upcoming",
+  popular: "popular",
+  top_rated: "top_rated",
+};
+
+export const tvType: { [key: string]: string } = {
+  upcoming: "popular",
+  popular: "top_rated",
+  top_rated: "on_the_air",
+};
+
+interface TmdbApi {
+  getMoviesList(type: string, params: object): Promise<any>;
+  getTvList(type: string, params: object): Promise<any>;
+  getVideos(cate: string, id: number): Promise<any>;
+  search(cate: string, params: object): Promise<any>;
+  detail(cate: string, id: number, params: object): Promise<any>;
+  credits(cate: string, id: number): Promise<any>;
+  similar(cate: string, id: number): Promise<any>;
+}
+
+const tmdbApi: TmdbApi = {
+  getMoviesList: (type: string, params: object) => {
+    const url = "movie/" + movieType[type];
+    return axiosClient.get(url, { params });
+  },
+  getTvList: (type: string, params: object) => {
+    const url = "tv/" + tvType[type];
+    return axiosClient.get(url, { params });
+  },
+  getVideos: (cate: string, id: number) => {
+    const url = category[cate] + "/" + id + "/videos";
+    return axiosClient.get(url, { params: {} });
+  },
+  search: (cate: string, params: object) => {
+    const url = "search/" + category[cate];
+    return axiosClient.get(url, { params });
+  },
+  detail: (cate: string, id: number, params: object) => {
+    const url = category[cate] + "/" + id;
+    return axiosClient.get(url, { params });
+  },
+  credits: (cate: string, id: number) => {
+    const url = category[cate] + "/" + id + "/credits";
+    return axiosClient.get(url, { params: {} });
+  },
+  similar: (cate: string, id: number) => {
+    const url = category[cate] + "/" + id + "/similar";
+    return axiosClient.get(url, { params: {} });
+  },
+};
+
+export default tmdbApi;
