cacd66357aaf6a0edfdb307f5bffc0f0bddea575
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-02-24 14:31:03 +0100

    Added MovieCard component with Props interface

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import apiConfig from '../api/apiConfig';
+import { category } from '../api/tmdbApi';
+
+interface Props {
+  item: {
+    id: number;
+    title: string;
+    name: string;
+    poster_path: string;
+    backdrop_path: string;
+  };
+  category: string;
+}
+
+function MovieCard(props: Props) {
+  const item = props.item;
+  const link = '/' + category[props.category] + '/' + item.id;
+  const bg = apiConfig.w500Image(item.poster_path || item.backdrop_path);
+
+  return (
+    <Link to={link}>
+      <div style={{ backgroundImage: `url(${bg})` }}>
+        <button>
+          <i>Play</i>
+        </button>
+      </div>
+      <h3>{item.title || item.name}</h3>
+    </Link>
+  );
+}
+
+export default MovieCard;
