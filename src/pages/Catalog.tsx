490e361e55878504a6f0e705bd6e36a323feab07
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-02-27 10:03:38 +0100

    Initial setup for PageHeader & catalog page

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -1,8 +1,20 @@
+import { useParams } from "react-router";
+import PageHeader from "../components/PageHeader";
+
+import { category as cate } from "../api/tmdbApi";
 
 function Catalog() {
+  const { category } = useParams();
+
+  console.log(category);
+
   return (
-    <div>Catalog</div>
-  )
+    <>
+      <PageHeader>
+        {category === cate.movie ? "Movies" : "TV series"}
+      </PageHeader>
+    </>
+  );
 }
 
-export default Catalog
+export default Catalog;
