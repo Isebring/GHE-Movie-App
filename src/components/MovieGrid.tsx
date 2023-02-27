5a00b27edbc262d4b9e8a2df78c7f954da192ca6
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-02-27 13:37:09 +0100

    added "load more" functionality & typed up interface

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -5,11 +5,18 @@ import tmdbApi, { category, movieType, tvType } from "../api/tmdbApi";
 import MovieCard from "./MovieCard";
 
 interface Props {
-  category: any;
+  item: {
+    id: number;
+    title: string;
+    name: string;
+    poster_path: string;
+    backdrop_path: string;
+  };
+  category: string;
 }
 
 function MovieGrid(props: Props) {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState<Props["item"][]>([]);
 
   const [page, setPage] = useState(1);
   const [totalPage, setTotalPage] = useState(0);
@@ -17,7 +24,7 @@ function MovieGrid(props: Props) {
   const { keyword } = useParams();
 
   useEffect(() => {
-    const getList = async () => {
+    async function getList() {
       let response = null;
       if (keyword === undefined) {
         const params = {};
@@ -36,15 +43,47 @@ function MovieGrid(props: Props) {
       }
       setItems(response.results);
       setTotalPage(response.total_pages);
-    };
+    }
     getList();
   }, [props.category, keyword]);
+
+  async function loadMore() {
+    let response = null;
+    if (keyword === undefined) {
+      const params = {
+        page: page + 1,
+      };
+      switch (props.category) {
+        case category.movie:
+          response = await tmdbApi.getMoviesList(movieType.upcoming, params);
+          break;
+        default:
+          response = await tmdbApi.getTvList(tvType.popular, params);
+      }
+    } else {
+      const params = {
+        page: page + 1,
+        query: keyword,
+      };
+      response = await tmdbApi.search(props.category, params);
+    }
+    setItems([...items, ...response.results]);
+    setPage(page + 1);
+  }
+
   return (
-    <Grid>
-      {items.map((item, i) => (
-        <MovieCard category={props.category} item={item} key={i} />
-      ))}
-    </Grid>
+    <>
+      <Grid>
+        {items.map((item, i) => (
+          <MovieCard category={props.category} item={item} key={i} />
+        ))}
+      </Grid>
+      {page < totalPage ? (
+        <div>
+          <button onClick={loadMore}>Load more</button>
+        </div>
+      ) : null}
+    </>
   );
 }
 
