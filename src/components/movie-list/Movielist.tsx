36b63c73f640c9b9045e53789c83cc62ae4cc705
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-02-27 10:06:11 +0100

    changed name from MovielistProps to Props. Updated Movie interface. Imported  MovieCard component

--- src/components/movie-list/Movielist.tsx
+++ src/components/movie-list/Movielist.tsx
@@ -1,63 +1,57 @@
 import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import apiConfig from '../../api/apiConfig';
 import tmdbApi, { category } from '../../api/tmdbApi';
+import MovieCard from '../MovieCard';
 
+interface Props {
+  category: string;
+  type: string;
+  id: number;
+}
 
-    interface MovielistProps {
-        category: string;
-        type: string;
-        id: number;
-    }
-    
+function Movielist(props: Props) {
+  const [items, setItems] = useState([]);
 
-function Movielist(props: MovielistProps) {
-    const [items, setItems] = useState([]);
+  interface Movie {
+    id: number;
+    title: string;
+    name: string;
+    poster_path: string;
+    backdrop_path: string;
+  }
 
+  useEffect(() => {
+    const getList = async () => {
+      let response = null;
+      const params = {};
 
-    interface Movie {
-        poster_path: string;
-    }
-    
-
-    useEffect(() => {
-        const getList = async () => {
-            let response = null;
-
-            if (props.type !== 'similar') {
-                switch(props.category) {
-                    case category.movie:
-                        response = await tmdbApi.getMoviesList(props.type, {params: Object});
-                        break;
-                        default:
-                            response = await tmdbApi.getTvList(props.type, {params: Object});
-                }
-            } else {
-                response = await tmdbApi.similar(props.category, props.id);
-            }
-            setItems(response.results);
+      if (props.type !== 'similar') {
+        switch (props.category) {
+          case category.movie:
+            response = await tmdbApi.getMoviesList(props.type, { params });
+            break;
+          default:
+            response = await tmdbApi.getTvList(props.type, { params });
         }
-        getList();
-    }, []);
+      } else {
+        response = await tmdbApi.similar(props.category, props.id);
+      }
+      setItems(response.results);
+    };
+    getList();
+  }, []);
 
-    return(
-        <div>
-            <Swiper
-                grabCursor={true}
-                spaceBetween={10}
-                slidesPerView={'auto'}
-                >
-                {
-                items.map((item: Movie, i: number) => (
-                    <SwiperSlide key={i}>
-                        <img src={apiConfig.w500Image(item.poster_path)} alt="" />
-                    </SwiperSlide>
-
-                ))
-                }   
-            </Swiper>
-        </div>
-    );
+  return (
+    <div>
+      <Swiper grabCursor={true} spaceBetween={10} slidesPerView={'auto'}>
+        {items.map((item: Movie, i: number) => (
+          <SwiperSlide key={i}>
+            <MovieCard item={item} category={props.category} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
 
 export default Movielist;
