ce20e29b0a5ac9a00c2f5baba0cf8b80bc20a0b4
Hampus Isebring <hampus.isebring@gmail.com>
2023-02-27 09:27:34 +0100

    Fixed API imgs

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -6,8 +6,8 @@ const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3",
   apiKey: "f4dff989aeebe08758854a530c47abf3",
   originalImage: (imgPath: string) =>
-    `https://image.tmbd.org/t/p/original/${imgPath}`,
-  w500Image: (imgPath: string) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+    `https://image.tmdb.org/t/p/original${imgPath}`,
+  w500Image: (imgPath: string) => `https://image.tmdb.org/t/p/w500${imgPath}`,
 };
 
 export default apiConfig;
