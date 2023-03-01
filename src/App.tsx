140a74e18568869569aa56db7ade796aeae962ce
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-03-01 21:42:44 +0100

    Added different styling and button behaviour for the different ErrorBoundaries

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,33 @@ import ErrorBoundary from "./ErrorBoundary";
 function App() {
   return (
     <>
-      <ErrorBoundary>
+      <ErrorBoundary
+        errorInfo={{
+          message: "Error in header",
+          variant: "small",
+        }}
+      >
         <Header />
       </ErrorBoundary>
-      <ErrorBoundary>
+      <ErrorBoundary
+        errorInfo={{
+          message:
+            "It looks like you have encountered a bug that has caused an error. Please try returning to the previous page by clicking the button below.",
+          image:
+            "https://cdn-icons-png.flaticon.com/512/1034/1034532.png?w=826&t=st=1677700431~exp=1677701031~hmac=a81e4bf668fd026ced8900a35d87ab6acd880d5287b84544de6ea45dc65ee098",
+          variant: "main",
+        }}
+      >
         <main>
           <Outlet />
         </main>
       </ErrorBoundary>
-      <ErrorBoundary>
+      <ErrorBoundary
+        errorInfo={{
+          message: "Error in footer",
+          variant: "small",
+        }}
+      >
         <Footer />
       </ErrorBoundary>
     </>
--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -3,6 +3,11 @@ import styled from "styled-components";
 
 interface Props {
   children: ReactNode;
+  errorInfo?: {
+    message?: string;
+    image?: string;
+    variant: "small" | "main";
+  };
 }
 
 interface State {
@@ -20,18 +25,78 @@ class ErrorBoundary extends Component<Props, State> {
   }
 
   render() {
+    const { image, message, variant } = this.props.errorInfo || {};
+
     if (this.state.hasError) {
-      return <Styledh1>Something went wrong...</Styledh1>;
+      switch (variant) {
+        case "small":
+          return (
+            <StyledSmallError>
+              <h2>{message}</h2>
+              <button onClick={() => window.history.back()}>
+                Refresh Page
+              </button>
+            </StyledSmallError>
+          );
+        case "main":
+          return (
+            <StyledMainError>
+              {image && <img src={image} alt="Error" />}
+              <p>{message}</p>
+              <button onClick={() => window.history.back()}>Go back</button>
+            </StyledMainError>
+          );
+        default:
+          return <div>Error</div>;
+      }
     }
 
     return this.props.children;
   }
 }
 
-const Styledh1 = styled.h1`
-  text-align: center;
+const StyledMainError = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   font-size: 1.5rem;
-  color: white;
+  gap: 2rem;
+  margin: 5rem 1rem;
+
+  img {
+    border-radius: 3%;
+    max-width: 100%;
+  }
+
+  p {
+    color: #fff;
+    font-size: 1.3rem;
+    font-family: system-ui, Arial, sans-serif;
+    text-align: center;
+  }
+
+  button {
+    padding: 1rem;
+  }
 `;
 
+const StyledSmallError = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  justify-content: center;
+  margin: 1rem;
+
+  h2 {
+    color: #fff;
+    font-size: 1.5rem;
+    font-family: system-ui, Arial, sans-serif;
+    margin: 0 0.5rem;
+  }
+
+  button {
+    padding: 0.3rem;
+  }
+`;
 export default ErrorBoundary;
