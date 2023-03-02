2ba4b5f15fa84f084d0ccaff61a8028ce56a6350
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-03-02 09:28:26 +0100

    prettier

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,50 +10,45 @@ function Home() {
     <div>
       <HeroSlide />
       <SpaceBetween>
-      <h2>Trending Movies</h2>
-      <OutlineButton>View more</OutlineButton>
+        <h2>Trending Movies</h2>
+        <OutlineButton>View more</OutlineButton>
       </SpaceBetween>
       <MovieList category={category.movie} type={movieType.popular} />
-      <Link to="/movie">
-      </Link>
-
+      <Link to="/movie"></Link>
 
       <SpaceBetween>
-      <h2>Top Rated Movies</h2>
-      <OutlineButton>View more</OutlineButton>
+        <h2>Top Rated Movies</h2>
+        <OutlineButton>View more</OutlineButton>
       </SpaceBetween>
       <MovieList category={category.movie} type={movieType.top_rated} />
-      <Link to="/movie">
-      </Link>
+      <Link to="/movie"></Link>
 
       <SpaceBetween>
-      <h2>Trending TV Shows</h2>
-      <OutlineButton>View more</OutlineButton>
+        <h2>Trending TV Shows</h2>
+        <OutlineButton>View more</OutlineButton>
       </SpaceBetween>
       <MovieList category={category.tv} type={tvType.popular} />
-      <Link to="/tv">
-      </Link>
+      <Link to="/tv"></Link>
 
       <SpaceBetween>
-      <h2>Top Rated TV Shows</h2>
-      <OutlineButton>View more</OutlineButton>
+        <h2>Top Rated TV Shows</h2>
+        <OutlineButton>View more</OutlineButton>
       </SpaceBetween>
       <MovieList category={category.tv} type={tvType.top_rated} />
-      <Link to="/tv">
-      </Link>
+      <Link to="/tv"></Link>
     </div>
   );
 }
 
 const SpaceBetween = styled.div`
-display: flex;
-justify-content: space-between;
-padding: 1rem;
-color: white;
-font-family: 'Poppins',system-ui, Arial, sans-serif;
-align-items: center;
-@media (max-width: 850px) {
-  flex-direction: column;
-}
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+  color: white;
+  font-family: 'Poppins', system-ui, Arial, sans-serif;
+  align-items: center;
+  @media (max-width: 850px) {
+    flex-direction: column;
+  }
 `;
 export default Home;
